21c214380ec4be783538fc62579e177640f16d69
victor <vchweya@avicdesign.com>
2018-06-07 13:10:07 +0300

    first commit

---
+++ designs.js
@@ -0,0 +1,38 @@
+$(function() {
+
+	var color, width, height;
+	// Select color input
+	color = $('#colorPicker').val();
+	
+	// Select size input
+	width = $('#inputWidth');
+	height = $('#inputHeight');
+
+	// When size is submitted by the user, call makeGrid()
+	$( "#sizePicker" ).submit(function() {
+	  makeGrid();
+	  event.preventDefault();
+	});
+
+	function makeGrid() {
+	// Your code goes here!
+	var tableRow, tableCell;
+  	for(var row = 0; row < height.val(); row++){
+  		tableRow = $('<tr></tr>');
+  		$('#pixelCanvas').append(tableRow);
+  		for(var col = 0; col < width.val(); col++){
+  			tableCell = $('<td></td>');
+  			tableRow.append(tableCell);
+
+  		}
+  	}
+  	$('#colorPicker').change(function() {
+  		color = $(this).val();
+  	})
+
+  	$('td').click(function() {
+  		$(this).css('background', color);
+  	})	 
+	}
+
+})
